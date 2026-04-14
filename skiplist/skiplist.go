bd2eebfa692363ca9dd6565e4327e10c5429a60d
Cameron Badman <cbadwork@gmail.com>
2026-04-14 10:01:31 +1000

    added basic delte method

--- skiplist/skiplist.go
+++ skiplist/skiplist.go
@@ -26,18 +26,18 @@ func (sl *Skiplist) Insert(value Data) {
 	if value == nil {
 		panic("cannot insert nil value")
 	}
-	update := sl.traverse(value)
+	update := sl.Traverse(value, sl.compare)
 	level := sl.randomLevel()
 	node := NewNode(value, level+1)
 	sl.insertNode(node, update[:level+1])
 }
 
 // traverses using a arg comparable a < value < b returns traversed levels
-func (sl *Skiplist) traverse(data Data) []*Node {
+func (sl *Skiplist) Traverse(data Data, compare func(a, b Data) int) []*Node {
 	update := make([]*Node, sl.maxLevel)
 	ptr := sl.head
 	for level := sl.maxLevel - 1; level >= 0; level-- {
-		for ptr.next[level] != nil && sl.compare(ptr.next[level].data, data) < 0 {
+		for ptr.next[level] != nil && compare(ptr.next[level].data, data) < 0 {
 			ptr = ptr.next[level]
 		}
 		update[level] = ptr
@@ -50,6 +50,25 @@ func (sl *Skiplist) insertNode(node *Node, insertPoints []*Node) error {
 	for index, n := range insertPoints {
 		node.next[index] = n.next[index]
 		n.next[index] = node
+	}
+
+	return nil
+}
+
+// Delete finds first insance of node them deletes it, requires compare
+func (sl *Skiplist) Delete(data Data, compare func(a, b Data) int) error {
+	update := sl.Traverse(data, compare)
+	target := update[0].next[0]
+
+	if target == nil || compare(target.data, data) != 0 {
+		return nil
+	}
+
+	for i := 0; i < sl.maxLevel; i++ {
+		if update[i].next[i] != target {
+			break
+		}
+		update[i].next[i] = target.next[i]
 	}
 
 	return nil
